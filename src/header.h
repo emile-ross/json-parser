1b48270d26b4c97517f059037025b22be5aa277e
emileross <emileross@tutanota.de>
2026-09-18 17:36:19 -0400

    fix: stack smashing in parser() function
    move variables to the global scope

--- src/header.h
+++ src/header.h
@@ -6,7 +6,7 @@
 
 /* parse.c */
 	int json_parse(const char *file_path, uint16_t num_entries, json_data json_entry[]);
-	uint16_t key_match(Bool *fail, const char *key_value, uint16_t num_entries, json_data json_entry[]);
+	uint16_t key_match(Bool *success, const char *key_value, uint16_t num_entries, json_data json_entry[]);
 
 /* compare.c */
 	Bool str_compare(const char *arg, const char *str);
--- src/parser.c
+++ src/parser.c
@@ -5,17 +5,21 @@ int json_parse(const char *file_path, uint16_t num_entries, json_data json_entry
 	FILE *fp = fopen(file_path, "r");
 	char line[256] = {0};	/* used for storing the line buffer in the file */
 	uint16_t current_lookup = 0;
+	uint16_t line_size = 0;
+	Bool open_quote = False;
+	uint16_t start_quote_index = 0;
+	uint16_t i = 0;
+	uint16_t j = 0;
+	uint32_t str_size = 0;
+	char *key_value = NULL;
+
 	file_check(fp, file_path);	/* checks for fp being NULL */
 
 	do {
+		Bool success = False;
+		open_quote = False;
 		/* only valid since line is an array of chars */
-		uint16_t line_size = sizeof(line);
-		Bool open_quote = False;
-		uint8_t start_quote_index = 0;
-		uint16_t i = 0;
-		uint16_t j = 0;
-		uint32_t str_size = 0;
-		char *key_value = NULL;
+		line_size = sizeof(line);
 
 		if (fgets(line, line_size, fp) == NULL)
 		{
@@ -23,12 +27,13 @@ int json_parse(const char *file_path, uint16_t num_entries, json_data json_entry
 			break;
 		}
 
-		for (; i < line_size; i++)
+		for (i = 0; i < line_size; i++)
 		{
 			if (line[i] == '"')
 			{
 				if (!(open_quote))
 				{
+					start_quote_index = i;
 					open_quote = True;
 				}
 				else
@@ -41,8 +46,7 @@ int json_parse(const char *file_path, uint16_t num_entries, json_data json_entry
 						key_value[i] = line[j];
 					}
 
-					Bool fail = False;
-					key_match(&fail, key_value, num_entries, json_entry);
+					key_match(&success, key_value, num_entries, json_entry);
 				}
 			}
 		}
@@ -56,19 +60,21 @@ int json_parse(const char *file_path, uint16_t num_entries, json_data json_entry
 	return 0;
 }
 
-uint16_t key_match(Bool *fail, const char *key_value, uint16_t num_entries, json_data json_entry[])
+uint16_t key_match(Bool *success, const char *key_value, uint16_t num_entries, json_data json_entry[])
 {
 	uint16_t i = 0;
 	for (i = 0; i < num_entries; i++)
 	{
 		if (str_compare(key_value, json_entry->key_value))
 		{
+			*(success) = False;
 			return i;
 		}
 	}
 
 	fprintf(stderr, "failed to find the value: %s\n", key_value);
 	exit(1);
-
+	
+	*(success) = False;
 	return 65535;
 }
