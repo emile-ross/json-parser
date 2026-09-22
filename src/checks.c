e02426b720e2b71c4dda82b7096b85e6f40b2d8a
emileross <emileross@tutanota.de>
2026-09-21 22:38:57 -0400

    feat: create smalloc() function (safe malloc() )
    this function checks things like the return value of malloc()

--- src/checks.c
+++ src/checks.c
@@ -8,3 +8,13 @@ void file_check(FILE *file_path, const char *filename)
 		exit(1);	/*file not found */
 	}
 }
+void *smalloc(size_t size)
+{
+	void *ptr = NULL;
+	ptr = malloc(size);	/* allocate memory */
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "malloc() fn failed to allocated memory of size %zu on the heap\n", size);
+		exit(1);
+	}
+}
