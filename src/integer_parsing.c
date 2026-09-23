8c8f7ed8697d3b79a8ea512ecfbfb3d35817a6a7
emileross <emileross@tutanota.de>
2026-09-22 22:02:02 -0400

    feat: integer_parsing.c: error checking & bounds
    add checks for integer bounds and invaid integers in strings (endptr)

--- src/integer_parsing.c
+++ src/integer_parsing.c
@@ -1,11 +1,26 @@
 #include "header.h"
 
 
-void parse_integer(const char *str, int32_t *value)
+void parse_integer(const char *str, int32_t *result)
 {
 	char *endptr;
 	long value;
 
 	value = strtol(str, &endptr, 10);
+
+	if (endptr == str)
+	{
+		fprintf(stderr, "No valid integer found in \"%s\" string \n", str);
+		return False;
+	}
+
+	if (value < INT32MIN || value > INT32MAX)
+	{
+		fprintf(stderr, "Integer is out of bounds (min: %ld) (max: %ld) input value: %ld\n", INT32MIN, INT32MAX, value);
+		return False;
+	}
+
 	*result = (int32_t)value;
+
+	return True;
 }
